0f53bc859918f7d1e9a11b96035c2042a4382131
youngseo.kim <kys6879@gmail.com>
2025-01-19 01:05:19 +0900

    스케줄생성

--- src/main/java/com/team3/gdgoc/schedule/AddScheduleRequest.java
+++ src/main/java/com/team3/gdgoc/schedule/AddScheduleRequest.java
@@ -1,5 +1,6 @@
 package com.team3.gdgoc.schedule;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,16 +9,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddScheduleRequest {
 
+    @Schema(description = "사용자 식별자(16자리랜덤문자열)", example = "abcdfqweaxvaweda")
     private String userIdentity;
 
+    @Schema(description = "일정 제목", example = "이력서 작성")
     private String title;
 
+    @Schema(description = "시작일", example = "2021-08-01")
     private String startDate;
 
+    @Schema(description = "종료일", example = "2021-08-02")
     private String endDate;
 
+    @Schema(description = "필수할 일", example = "이력서 작성")
     private String mustDoTasks;
 
+    @Schema(description = "일정 요구사항", example = "이력서 작성")
     private String requirements;
 
     @Builder
