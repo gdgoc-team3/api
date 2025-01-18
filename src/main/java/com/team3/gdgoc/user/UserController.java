21681c876a0e089aa2dac9143aba353b88415ae3
hyejin <hyejin66@sookmyung.ac.kr>
2025-01-18 23:51:59 +0900

    mock

--- src/main/java/com/team3/gdgoc/user/UserController.java
+++ src/main/java/com/team3/gdgoc/user/UserController.java
@@ -3,6 +3,7 @@ package com.team3.gdgoc.user;
 import com.team3.gdgoc.common.ApiResponse;
 import java.time.LocalDate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 //    private final UserService userService;
 
-    @GetMapping("")
-    public ApiResponse<UserInfoResponse> getUserInfo() {
+    @GetMapping("/{userIdentity}")
+    public ApiResponse<UserInfoResponse> getUserInfo(@PathVariable String userIdentity) {
         UserInfoResponse response = UserInfoResponse.builder()
                 .birthDate(LocalDate.of(1998, 1, 1))
                 .nickname("개발자")
-                .userIdentity("user12324")
+                .userIdentity(userIdentity)
                 .major("컴퓨터과학과")
                 .desiredJob("백엔드")
                 .targetEmploymentPeriod(3)
