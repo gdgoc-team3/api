663964558ca933597f08ce6ec900069242166fb7
youngseo.kim <kys6879@gmail.com>
2025-01-18 21:11:41 +0900

    entity

--- src/main/java/com/team3/gdgoc/schedule/ScheduleService.java
+++ src/main/java/com/team3/gdgoc/schedule/ScheduleService.java
@@ -1,4 +1,21 @@
 package com.team3.gdgoc.schedule;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    public ScheduleEntity createSchedule(ScheduleEntity scheduleEntity) {
+        return scheduleRepository.save(scheduleEntity);
+    }
+
+    public List<ScheduleEntity> getScheduleList() {
+        return scheduleRepository.findAll();
+    }
 }
