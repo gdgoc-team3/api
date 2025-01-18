438ffd96b1318a60ef721af560c20354d885967f
youngseo.kim <kys6879@gmail.com>
2025-01-19 00:17:02 +0900

    AddScheduleRequest

--- src/main/java/com/team3/gdgoc/schedule/AddScheduleRequest.java
+++ src/main/java/com/team3/gdgoc/schedule/AddScheduleRequest.java
@@ -2,9 +2,13 @@ package com.team3.gdgoc.schedule;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AddScheduleRequest {
+
+    private String userIdentity;
 
     private String title;
 
@@ -17,7 +21,8 @@ public class AddScheduleRequest {
     private String requirements;
 
     @Builder
-    public AddScheduleRequest(String title, String startDate, String endDate, String mustDoTasks, String requirements) {
+    public AddScheduleRequest(String userIdentity, String title, String startDate, String endDate, String mustDoTasks, String requirements) {
+        this.userIdentity = userIdentity;
         this.title = title;
         this.startDate = startDate;
         this.endDate = endDate;
