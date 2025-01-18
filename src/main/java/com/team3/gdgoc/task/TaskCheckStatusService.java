fefdfca0d71c9211669f9aa75b080cc56635939a
hini5971 <sini5971@gmail.com>
2025-01-19 05:32:13 +0900

    mock

--- src/main/java/com/team3/gdgoc/task/TaskCheckStatusService.java
+++ src/main/java/com/team3/gdgoc/task/TaskCheckStatusService.java
@@ -6,10 +6,7 @@ import com.team3.gdgoc.schedule.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
